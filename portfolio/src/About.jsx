8543db18210212b2e9a6173ae2775727bb905b8b
BATHULA NAGA SAI KIRAN <bathulasaikiran2k2@gmail.com>
2025-05-27 03:51:00 +0530

    Update portfolio project with latest changes in Projects.jsx and images

--- portfolio/src/About.jsx
+++ portfolio/src/About.jsx
@@ -1,10 +1,10 @@
-import dp from "./dp.png";
+import profile from "/src/dp.png";
 import "./About.css";
 
 function About() {
     return (
         <div id="about">
-            <img src={dp} alt=""/>
+            <img src={profile} alt=""/>
             <div id="about-para">
                 <p>I’m <span>Naga Sai Kiran</span>, a Computer Science and Engineering student passionate about full-stack web development and software engineering. I specialize in the MERN stack and enjoy building real-world applications that solve meaningful problems. I'm focused on growing my skills and aiming for roles at top tech companies, combining technical knowledge with a drive for continuous learning and innovation.</p>
             </div>
