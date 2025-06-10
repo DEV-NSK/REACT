9882dc737ac82ad5f75dc5a5810d750025badebb
BATHULA NAGA SAI KIRAN <bathulasaikiran2k2@gmail.com>
2025-06-10 21:21:17 +0530

    Fix: Ensure Signup folder and file use correct casing for Vercel

--- randomclr/src/App/App.jsx
+++ randomclr/src/App/App.jsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import Navbar from '/src/Navbar/Navbar.jsx';
-import {Route , Routes} from 'react-router-dom';
-import Randomclr from '/src/Randomclr/Randomclr.jsx';
-import TeluguQuotes from '/src/TeluguQuotes/TeluguQuotes.jsx';
+import React from "react";
+import Navbar from "/src/Navbar/Navbar.jsx";
+import { Route, Routes } from "react-router-dom";
+import Randomclr from "/src/Randomclr/Randomclr.jsx";
+import TeluguQuotes from "/src/TeluguQuotes/TeluguQuotes.jsx";
 // import ThemeToggler from '/src/ThemeTogglr/Themetglr.jsx';
-import Themetglr from '/src/ThemeTogglr/Themetglr.jsx'
-import SignUp from '/src/SignUp/SignUp.jsx';
+import Themetglr from "/src/ThemeTogglr/Themetglr.jsx";
+import Signup from "/src/Signup/Signup.jsx";
 
 class App extends React.Component {
-    render() {
-        return (
-            <div>
-                <Navbar />
-                <Routes>
-                    <Route path='/' element={<Randomclr/>}/>
-                    <Route path='/TeluguQuotes' element={<TeluguQuotes/>}/>
-                    <Route path='/ThemeToggler' element={<Themetglr/>}/>
-                    <Route path='/SignUp' element={<SignUp/>}/>
-                </Routes>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Randomclr />} />
+          <Route path="/TeluguQuotes" element={<TeluguQuotes />} />
+          <Route path="/ThemeToggler" element={<Themetglr />} />
+          <Route path="/SignUp" element={<Signup />} />
+        </Routes>
+      </div>
+    );
+  }
 }
 
 export default App;
